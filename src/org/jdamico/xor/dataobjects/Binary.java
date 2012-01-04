71f0f05fb7c2b24286bd5177e3947b09a3c80946
jdamico <jdamico@debian.(none)>
2012-01-03 19:25:35 -0500

    initial commit

---
+++ src/org/jdamico/xor/dataobjects/Binary.java
@@ -0,0 +1,27 @@
+package org.jdamico.xor.dataobjects;
+
+public class Binary {
+	
+	private int[] bitArray = new int[4];
+	private String strBitArray;
+	
+	
+	public int[] getBitArray() {
+		return bitArray;
+	}
+
+	public void setBitArray(int[] bitArray) {
+		this.bitArray = bitArray;
+	}
+
+	public String getStrBitArray() {
+		return strBitArray;
+	}
+
+	public void setStrBitArray(String strBitArray) {
+		this.strBitArray = strBitArray;
+	}
+
+	
+	
+}
